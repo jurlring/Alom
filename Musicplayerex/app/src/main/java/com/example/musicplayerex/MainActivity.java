dd40f868a6e20bf68e19c83d14ff5fa32bdc5925
jurlring <78091011+jurlring@users.noreply.github.com>
2021-08-18 18:03:49 +0900

    music player

---
+++ Musicplayerex/app/src/main/java/com/example/musicplayerex/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.musicplayerex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_play;
+    Button btn_stop;
+
+
+    MediaPlayer mediaPlayer;
+
+    // 액티비티가 종료될때.. 이곳을 실행함.
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btn_play = findViewById(R.id.btn_play);
+        btn_stop = findViewById(R.id.btn_stop);
+
+        // 재생버튼 눌렀을때..
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.friday);
+                mediaPlayer.start();
+            }
+        });
+
+
+        // 정지버튼 눌렀을때..
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying()) {//음악이 재생중인지 검사
+                    mediaPlayer.stop();
+                    mediaPlayer.reset();
+                }
+            }
+        });
+    }
+}
